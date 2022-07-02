01ade033589b8aed4e8f0a074dcd2e7ab8677ae9
Francois Hill wootbook <francoishill11@gmail.com>
2022-07-02 20:38:02 +0200

    dev: minor code improvement

--- FunctionApp/Infrastructure/Factories/HttpResponseFactory.cs
+++ FunctionApp/Infrastructure/Factories/HttpResponseFactory.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FNB.InContact.Parser.FunctionApp.Infrastructure.Factories;
 
-public class HttpResponseFactory
+public static class HttpResponseFactory
 {
     public static IActionResult CreateBadRequestResponse(params string[] errors)
     {
         return new BadRequestObjectResult(new Dictionary<string, object>
         {
-            { "Errors", errors }
+            ["Errors"] = errors,
         });
     }
 }
